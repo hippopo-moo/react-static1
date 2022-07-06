8628f6d5c92333cecf7e27697e359768c70425eb
ykosugi <hippopo.moo@gmail.com>
2022-07-06 23:02:54 +0900

    feat: Fixed to retrieve articles from my blog.

--- api.js
+++ api.js
@@ -1,7 +1,11 @@
 export async function fetchArticles() {
     const response = await fetch(
-        `http://myjson.dit.upm.es/api/bins/igqb`
-    );
+        `https://hippopo-web.work/wp-json/wp/v2/posts?per_page=3`
+        // `https://myjson.dit.upm.es/api/bins/fee3`
+        // `https://dog.ceo/api/breed/shiba/images/random/12`
+        );
     const data = await response.json();
-    return data.message;
+    console.log(data);
+    return data;
+    // return data.message; //帰ってきたJSONはmessageに入ってる
 }
